3d906acfcffa05a6556d621d86947904ef18b7a1
gege0121 <evelynhuang@Xingyues-MacBook.local>
2020-01-21 13:00:42 -0500

    Add FileService Test

---
+++ src/main/java/com/ascending/training/service/FileService.java
@@ -0,0 +1,12 @@
+package com.ascending.training.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface FileService {
+    String uploadFile(String bucketName, MultipartFile file) throws IOException;
+    String getFileUrl(String bucketName, String fileName);
+    void createBucket(String bucketName);
+    public boolean saveFile(MultipartFile multipartFile, String filePath);
+}
